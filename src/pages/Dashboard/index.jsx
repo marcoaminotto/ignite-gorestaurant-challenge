d38da91950b2110e4b01c5e5c0f17664d0b4adab
Marco Echevestre <devminotto@gmail.com>
2021-07-26 14:15:10 +0200

    Migrate component Header to function component and typescript

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 import api from '../../services/api';
 import { Food } from '../../components/Food';
 import ModalAddFood from '../../components/ModalAddFood';
