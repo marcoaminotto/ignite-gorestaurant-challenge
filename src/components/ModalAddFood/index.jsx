838bdb38823b20581835a490edbbaaa2a3c8e81a
Marco Echevestre <devminotto@gmail.com>
2021-07-26 14:34:29 +0200

    Migrate Modal component to function component and typescript

--- src/components/ModalAddFood/index.jsx
+++ src/components/ModalAddFood/index.jsx
@@ -2,7 +2,7 @@ import { Component, createRef } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
-import Modal from '../Modal';
+import { Modal } from '../Modal';
 import Input from '../Input';
 
 class ModalAddFood extends Component {
@@ -12,7 +12,7 @@ class ModalAddFood extends Component {
     this.formRef = createRef();
   }
 
-  handleSubmit = async data => {
+  handleSubmit = async (data) => {
     const { setIsOpen, handleAddFood } = this.props;
 
     handleAddFood(data);
@@ -42,6 +42,6 @@ class ModalAddFood extends Component {
       </Modal>
     );
   }
-};
+}
 
 export default ModalAddFood;
