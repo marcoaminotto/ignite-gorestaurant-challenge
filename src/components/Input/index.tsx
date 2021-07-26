e4483a024ecd14ebe1c2e53250d8c693fe7e4ea5
Marco Echevestre <devminotto@gmail.com>
2021-07-26 15:15:24 +0200

    Migrate ModalAddFood component to function component and typescript

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -13,7 +13,7 @@ import { IconType } from 'react-icons';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string,
-  icon: IconType,
+  icon?: IconType,
 }
 
 const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
